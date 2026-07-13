bd4defb418650cb1102b5ea7a9e11cb871485fd9
SamuelPerezCO <sernasamuelperez@gmail.com>
2026-07-13 17:15:02 -0500

    Fixing the conversation handler

--- src/main.py
+++ src/main.py
@@ -10,8 +10,19 @@ load_dotenv()
 TOKEN_BOT = os.getenv("TOKEN_BOT")
 
 application = ApplicationBuilder().token(TOKEN_BOT).build()
-application.add_handler( CommandHandler("start" , tracker_controller.tracker_controller.who_are_you ) )
+conversation = ConversationHandler(
+    entry_points=[ CommandHandler("start" , tracker_controller.tracker_controller.who_are_you) ],
+    states={
+        tracker_controller.WHO_ARE_YOU: [ MessageHandler(filters.TEXT & ~filters.COMMAND , tracker_controller.tracker_controller.who_are_you_answer) ],
+        tracker_controller.MENU: [ MessageHandler(filters.TEXT & ~filters.COMMAND , tracker_controller.tracker_controller.report_new_day) ],
+    },
+    fallbacks=[ CommandHandler("start" , tracker_controller.tracker_controller.who_are_you) ],
+)
+application.add_handler(conversation)
+
 application.run_polling(allowed_updates=Update.ALL_TYPES)
+
+
 
 
 """
